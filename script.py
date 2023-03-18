2d2a201f0d671e79387ca064fa1bc226ecedb55e
Arman <65326024+tess-ss@users.noreply.github.com>
2023-03-18 15:01:31 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,11 @@
 import sys
 import json
 a=[]
+filename=sys.argv[1].split('/')[-1]
 with open(sys.argv[1],"r") as file:
     for line in file:
         a.append([line.rstrip()])
-d={"name":sys.argv[1],"domains":a}
+d={"name":filename,"domains":a}
 
-with open(f"{sys.argv[1]}.json","w") as outfile:
+with open(f"{filename}.json","w") as outfile:
     json.dump(d,outfile)
