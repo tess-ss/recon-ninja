730e03c8940c3ccb70f9df5432e1b4796fcc442f
talal x <122642354+als-alt@users.noreply.github.com>
2023-07-13 05:15:38 +0300

    Update Template.js

--- Backend/Models/Template.js
+++ Backend/Models/Template.js
@@ -1,15 +1,1 @@
-const { Schema,model }= require("mongoose")
 
-const TemplateSchema = new Schema({
-  name:{
-    required:true,
-    type:String
-  },
-  value:{
-    type:String
-  }
-},{
-  timestamps:true
-});
-
-module.exports=model("Template",TemplateSchema)
